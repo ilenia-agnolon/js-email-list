8ff942f5031cbde0c92d909823eadb9f20304316
ilemia-agnolon <ilenia.agnolon.96@gmail.com>
2025-09-15 17:22:28 +0200

    generato prima email casuale (email singola) creando un nuovo li dentro axios.get

--- js/script.js
+++ js/script.js
@@ -2,3 +2,32 @@
 const myResult = document.querySelector(".resultList");
 
 
+//salvo in una variabile l'idirizzo API di Boolean
+const endPointMail = "https://flynn.boolean.careers/exercises/api/random/mail";
+
+//faccio una chiamata all'API con axios
+axios.get(endPointMail)
+.then((response) => {
+
+    //quando la risposta arriva, apro il pacchetto JSON
+    //    e prendo solo la proprietà "response" = l’email
+    const email = response.data.response;
+
+
+//creo nuovo <li>
+const newLi = document.createElement("li");
+
+//scrivo l'email dentro al <li>
+newLi.innerText = email;
+
+//appendo il <li> alla lista <ul>
+myResult.appendChild(newLi);
+
+})
+
+.catch((error) => {
+    console.error("errore", error);
+    
+});
+
+
